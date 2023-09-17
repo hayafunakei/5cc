5c3523c12bdca1de656c54ea1457b36924cb27d1
hayafunakei <hayafuna@yahoo.co.jp>
2023-09-17 10:07:35 +0900

    単項演算子対応

--- 5cc.c
+++ 5cc.c
@@ -167,6 +167,7 @@ Node *new_node_num(int val) {
 // 前後するためプロトタイプ宣言
 Node *expr();
 Node *mul();
+Node *unary();
 Node *primary();
 
 // expr = mul ("+" mul | "-" mul)*
@@ -183,18 +184,28 @@ Node *expr() {
     }
 }
 
-// mul = primary ("*" primary | "/" primary)*
+// mul = unary ( "*" unary | "/" unary)*
 Node *mul() {
-    Node *node = primary();
+    Node *node = unary();
 
     for (;;) {
         if (consume('*'))
-            node = new_node(ND_MUL, node, primary());
+            node = new_node(ND_MUL, node, unary());
         else if (consume('/'))
-            node = new_node(ND_DIV, node, primary());
+            node = new_node(ND_DIV, node, unary());
         else
             return node;
     }
+}
+
+// unary = ("+" | "-")? unary | primary 
+Node *unary() {
+    if (consume('+'))
+        return unary();
+    if (consume('-'))
+        // 0と右ノードを引き算してマイナスにする。
+        return new_node(ND_SUB, new_node_num(0), unary());
+    return primary();
 }
 
 // primary = "(" expr ")" | num
